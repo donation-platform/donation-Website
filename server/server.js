03a60ff7c082615f869cb7725dd33bb4d4a61f51
l2vrns <lorans0alkhalailah@gmail.com>
2025-02-27 19:50:49 +0300

    done

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log("object")
+const express = require("express");
+
+const PORT = 5000;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("a7a");
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening to port ${PORT}`);
+});
